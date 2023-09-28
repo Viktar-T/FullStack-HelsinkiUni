bf55721de37b9770f4cbdf21a61a0657b932bf02
Viktar Taustyka <taustyka.viktar@gmail.com>
2023-09-28 17:29:38 +0200

    1.1-1.2-clean

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -14,68 +14,24 @@ const Part = (props) => {
   )
 }
 
-const Part3 = (props) => {
-  return (
-    <p>
-      {props.name} {props.exercises}
-    </p>
-  )
-}
-
 const Content = (props) => {
   return (
     <div>
-      {props.part} {props.exercise}
-    </div>
-  )
-}
-
-const Content2 = (props) => {
-  return (
-    <div>
-      <Part3 part={props.allcontent[0].part} exercise={props.allcontent[0].exercises}/>
-      <Part3 part={props.allcontent[1].part} exercise={props.allcontent[1].exercises}/>
-      <Part3 part={props.allcontent[2].part} exercise={props.allcontent[2].exercises}/>
-    </div>
-  )
-}
-
-const Content3 = (props) => {
-  return (
-    <div>
-      {/* for (let i=0; i<props.allcontent.length -1; ) */}
       <Part name={props.parts[0].name} exercises={props.parts[0].exercises}/>
       <Part name={props.parts[1].name} exercises={props.parts[1].exercises}/>
       <Part name={props.parts[2].name} exercises={props.parts[2].exercises}/>
-
-
     </div>
   )
 }
 
+
 const Total = (props) => {
-  return (
-    <p>Number of exercises {props.total}</p>
-  )
-}
-
-const Total2 = (props) => {
-  /* const parts = props.parts
-  const total1 = parts[0].exercises + parts[1].exercises + parts[2].exercises;
-
-  let total2 = 0;
-  for (let i=0; i<parts.length; i++) {
-    total2 += parts[i].exercises
-  } */
-
   let total3 = 0;
   props.parts.forEach(element => {
     total3 += element.exercises
   });
   return (
     <>
-      {/* <p>Number of exercises {total1}</p>
-      <p>Number of exercises {total2}</p> */}
       <p>Number of exercises {total3}</p>
     </>
   ) 
@@ -102,8 +58,8 @@ const App = () => {
   return (
     <div>
       <Header name={course.name} />
-      <Content3 parts={course.parts} />
-      <Total2 parts={course.parts} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
