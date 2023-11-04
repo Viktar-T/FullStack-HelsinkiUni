712f02ee6dbca8c14ebd0862367b4e3b749ddf1b
Viktar Taustyka <taustyka.viktar@gmail.com>
2023-11-04 11:45:57 +0100

    2.20

--- part2/countries/src/services/service.js
+++ part2/countries/src/services/service.js
@@ -14,4 +14,17 @@ const getCountry = (name) => {
     )
 }
 
-export default {getAll, getCountry}
+const getWether = (lat, lon) => {
+    /* const lat = "33.44"
+    const lon = "-94.04" */
+    console.log("lat2:", lat, "  lon2:", lon)
+    const part = "hourly,daily"
+    const APIkey = "d3187454a4968eb3023067c47581da3b"
+
+    const request = axios
+    .get(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=${part}&appid=${APIkey}`)
+    
+    return request.then(resp => resp.data)
+}
+
+export default {getAll, getCountry, getWether}
