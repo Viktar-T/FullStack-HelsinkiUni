faa8a9fae46103b40471221c142cd74ca0aaf706
Viktar <taustyka.viktar@gmail.com>
2024-09-28 11:18:50 +0200

    C7

---
+++ js/one_js_exercises/obj.js
@@ -0,0 +1,11 @@
+const obj1 = {
+    left: 0,
+    right: 0,
+}
+
+console.log(obj1)
+
+obj1.left += 1;
+obj1.left += 1;
+obj1.left += 1;
+console.log(obj1)
--- part1-2024/complex-state/src/App.jsx
+++ part1-2024/complex-state/src/App.jsx
@@ -1,20 +1,53 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
+  const [clicks, setClicks] = useState({left: 0, right: 0})
+  const [allClicks, setAll] = useState([])
+
+  const handleClicks = (lr) => {
+    let newObj;
+    if (lr === "left") {
+      newObj = {left: clicks.left + 1, right: clicks.right}
+      setAll(allClicks.concat("L"))
+    } else {
+      newObj = {left: clicks.left, right: clicks.right + 1}
+      setAll(allClicks.concat("R"))
+    }
+      return newObj
+  };
+
+  const handleClicks2 = (lr) => {
+    let newObj;
+    if (lr === "left") {
+      newObj = {...clicks, left: clicks.left + 1,}
+      setAll(allClicks.concat("L"))
+    } else {
+        newObj = {...clicks, right: clicks.right + 1}
+        setAll(allClicks.concat("R"))
+      }
+      setClicks(newObj)
+  };
+
+  const handleLeftClick = () => {
+    const newClick = {left: clicks.left + 1, right: clicks.right}
+    setAll(allClicks.concat("L"))
+    setClicks(newClick)
+  }
+
+  console.log("clicks: ", clicks)
 
   return (
     <div>
-      {left}
-      <button onClick={() => setLeft(left + 1)}>
-        left
-      </button>
-      <button onClick={() => setRight(right + 1)}>
-        right
-      </button>
-      {right}
+      {clicks.left}
+      <button onClick={() => setClicks(handleClicks("left"))}>Left</button>
+      <button onClick={handleLeftClick}>Left</button>
+      <button onClick={() => handleClicks2("left")}>Left</button>
+      <button onClick={() => setClicks(handleClicks("right"))}>Right</button>
+      {clicks.right}  
+      <p>{allClicks.join("->")}</p>
+
     </div>
+    
   )
 }
 
